432819dea2856076c1e66ab16833a56c53f3ee3c
tahir826 <mtahirhassan826@gmail.com>
2024-10-20 12:54:51 +0500

    refine landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ function LandingPage() {
   className="min-h-screen flex flex-col justify-center items-center bg-black text-center px-4"
 >
   <div className="flex flex-col items-center">
-    <p className="text-5xl sm:text-6xl md:text-8xl bg-gradient-to-r from-blue-500 to-purple-700 bg-clip-text text-transparent font-bold leading-tight">
+    <p className="text-5xl sm:text-6xl md:text-8xl mt-10 bg-gradient-to-r from-blue-500 to-purple-700 bg-clip-text text-transparent font-bold leading-tight">
       HeadlineAI
     </p>
     <p className="text-white text-xl sm:text-2xl md:text-3xl mt-2">
@@ -115,7 +115,7 @@ function LandingPage() {
   </div>
 
   <div className="mt-6">
-    <p className="text-white text-lg sm:text-2xl md:text-5xl leading-relaxed mt-10">
+    <p className="text-white text-lg sm:text-2xl md:text-7xl leading-relaxed mt-10">
       Explore The<br/>Potential of <span className="text-blue-600">Agentic AI</span><br/>in news
     </p>
   </div>
